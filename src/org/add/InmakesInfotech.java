2e5b71107ba14b68e9569208e8dd8a50b812712b
vinothmanimaran <vinothmg28@gmail.com>
2023-05-10 19:31:24 +0530

    Updated registration functionality

---
+++ src/org/add/InmakesInfotech.java
@@ -0,0 +1,29 @@
+package org.add;
+
+public class InmakesInfotech {
+	private void inmakesTiruvendrum() {
+		System.out.println("Tiruvendrum");
+	}
+	private void inmakesKochi() {
+		System.out.println("Kochi");
+    }
+	private void inmakesChennai() {
+		System.out.println("Chennai");
+    }
+	private void inmakesBangalore() {
+		System.out.println("bangalore");
+    }
+	private void inmakesDubai() {
+		System.out.println("Dubai");
+
+	}
+	public static void main(String[] args) {
+		InmakesInfotech n=new InmakesInfotech();
+		n.inmakesTiruvendrum();
+		n.inmakesKochi();
+		n.inmakesChennai();
+		n.inmakesBangalore();
+		n.inmakesDubai();
+	}
+
+}
